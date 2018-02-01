61cf099774f79190f1551b83e7f3de63fca6e729
Unknown <hej.mellor@gmail.com>
2018-02-01 17:40:38 +0000

    Removed matches from wrong function

--- Eigen 2014/dataset.py
+++ Eigen 2014/dataset.py
@@ -25,12 +25,12 @@ class DataSet:
         image = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
         depth = tf.image.resize_images(depth, (TARGET_HEIGHT, TARGET_WIDTH))
         invalid_depth = tf.sign(depth)
-        images, depths, invalid_depths = tf.train.batch(
-            [image, depth, invalid_depth],
-            batch_size=self.batch_size,
-            num_threads=4,
-            capacity= 50 + 3 * self.batch_size,
-        )
+        #images, depths, invalid_depths = tf.train.batch(
+        #    [image, depth, invalid_depth],
+        #    batch_size=self.batch_size,
+        #    num_threads=4,
+        #    capacity= 50 + 3 * self.batch_size,
+        #)
         return images, depths, invalid_depths
 
     def csv_inputs(self, csv_file_path):
@@ -49,16 +49,16 @@ class DataSet:
         depth = tf.div(depth, [255.0])
         #depth = tf.cast(depth, tf.int64)
         # resize
-        images = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
-        depths = tf.image.resize_images(depth, (TARGET_HEIGHT, TARGET_WIDTH))
-        invalid_depths = tf.sign(depth)
+        image = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
+        depth = tf.image.resize_images(depth, (TARGET_HEIGHT, TARGET_WIDTH))
+        invalid_depth = tf.sign(depth)
         # generate batch
-        #images, depths, invalid_depths = tf.train.batch(
-    #        [image, depth, invalid_depth],
-    #        batch_size=self.batch_size,
-    #        num_threads=4,
-    #        capacity= 50 + 3 * self.batch_size,
-    #    )
+        images, depths, invalid_depths = tf.train.batch(
+            [image, depth, invalid_depth],
+            batch_size=self.batch_size,
+            num_threads=4,
+            capacity= 50 + 3 * self.batch_size,
+        )
         return images, depths, invalid_depths
 
 
