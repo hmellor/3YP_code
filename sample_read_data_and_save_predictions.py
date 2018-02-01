485e44791ae2c8adc3e91548275577b10ce2c172
Unknown <hej.mellor@gmail.com>
2018-02-01 15:20:18 +0000

    Removed plotting from Sample

--- sample_read_data_and_save_predictions.py
+++ sample_read_data_and_save_predictions.py
@@ -15,15 +15,14 @@ depth = data['depths'][:,:,imageIdx]
 #image2 = Image.fromarray(image, 'RGB')
 #image2.show()
 
-import matplotlib.pyplot as plt
-plt.imshow(image)
-plt.show()
-plt.imshow(depth)
-plt.show()
+#import matplotlib.pyplot as plt
+#plt.imshow(image)
+#plt.show()
+#plt.imshow(depth)
+#plt.show()
 
 print('Add a noise to depths and save them as predictions')
 predictions = copy.deepcopy(data['depths'])
 noise = numpy.random.normal(0, 0.5, data['depths'].shape)
 predictions += noise
 numpy.savez('predictions', depths=predictions)
-
