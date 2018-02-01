1de40cba3688d17301f0aad55f59936f513bae1c
Unknown <hej.mellor@gmail.com>
2018-02-01 18:45:52 +0000

    More fiddling

--- Eigen 2014/dataset.py
+++ Eigen 2014/dataset.py
@@ -16,17 +16,17 @@ class DataSet:
         data = np.load(npz_file_path)
         # input
         image = data['images']
+        image = np.moveaxis(image, [0,1,2,3], [3,0,1,2])
         image = tf.cast(image, tf.float32)
         # target
         depth = data['depths']
+        depth = np.moveaxis(image, [0,1,2], [2,0,1])
+        depth = np.expand_dims(depth,3)
         depth = tf.cast(depth, tf.float32)
         depth = tf.div(depth, [255.0])
         # shuffle columns
-        image = np.moveaxis(image, [0,1,2,3], [3,0,1,2])
-        depth = np.moveaxis(image, [0,1,2], [2,0,1])
         print(image.shape)
         print(depth.shape)
-        depth = np.expand_dims(depth,3)
         print(depth.shape)
         # resize
         image = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
