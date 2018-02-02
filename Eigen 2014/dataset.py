fde65e3ca20753b4d191ff0b3182005c3f810b44
Unknown <hej.mellor@gmail.com>
2018-02-02 10:05:52 +0000

    Removed batching

--- Eigen 2014/dataset.py
+++ Eigen 2014/dataset.py
@@ -28,16 +28,16 @@ class DataSet:
         image = tf.image.resize_images(image, (IMAGE_HEIGHT, IMAGE_WIDTH))
         depth = tf.image.resize_images(depth, (TARGET_HEIGHT, TARGET_WIDTH))
         invalid_depth = tf.sign(depth)
-        image = tf.unstack(image,1000,0)
-        depth = tf.unstack(image,1000,0)
-        invalid_depth = tf.unstack(image,1000,0)
+        images = tf.unstack(image,1000,0)
+        depths = tf.unstack(image,1000,0)
+        invalid_depths = tf.unstack(image,1000,0)
         # create batches
-        images, depths, invalid_depths = tf.train.batch(
-            [image, depth, invalid_depth],
-            batch_size=self.batch_size,
-            num_threads=4,
-            capacity= 32
-        )
+        #images, depths, invalid_depths = tf.train.batch(
+        #    [image, depth, invalid_depth],
+        #    batch_size=self.batch_size,
+        #    num_threads=4,
+        #    capacity= 32
+        #)
         return images, depths, invalid_depths
 
     def csv_inputs(self, csv_file_path):
