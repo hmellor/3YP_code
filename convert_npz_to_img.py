de67cc7c39715fed7f89a7644dc29af01d00e4ec
Unknown <hej.mellor@gmail.com>
2018-02-03 18:51:32 +0000

    Adapting root folder for gerneral cnn implementation

--- convert_npz_to_img.py
+++ convert_npz_to_img.py
@@ -6,21 +6,24 @@ from PIL import Image
 import random
 
 
-def convert_nyu(path):
+def convert_train(path):
     print("load dataset: %s" % (path))
-    f = h5py.File(path)
-
+    f = np.load(path)
+    images = f['images']
+    images = np.transpose(images, [3, 2, 1, 0])
+    depths = f['depths']
+    depths = np.transpose(depths, [2, 1, 0])
 
     trains = []
-    for i, (image, depth) in enumerate(zip(f['images'], f['depths'])):
+    for i, (image, depth) in enumerate(zip(images, depths)):
         ra_image = image.transpose(2, 1, 0)
         ra_depth = depth.transpose(1, 0)
         re_depth = (ra_depth/np.max(ra_depth))*255.0
         image_pil = Image.fromarray(np.uint8(ra_image))
         depth_pil = Image.fromarray(np.uint8(re_depth))
-        image_name = os.path.join("data", "nyu_datasets", "%05d.jpg" % (i))
+        image_name = os.path.join("data", sys.argv[1] "_datasets", "%05d.jpg" % (i))
         image_pil.save(image_name)
-        depth_name = os.path.join("data", "nyu_datasets", "%05d.png" % (i))
+        depth_name = os.path.join("data", sys.argv[1] "_datasets", "%05d.png" % (i))
         depth_pil.save(depth_name)
 
         trains.append((image_name, depth_name))
@@ -33,6 +36,14 @@ def convert_nyu(path):
             output.write("\n")
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+    	print("Please run:\n\tpython convert_npz_to_img.py <train/val/test>")
+    	exit()
+
+    if not os.path.isfile(sys.argv[1]):
+    	print(sys.argv[1] + " is not a file")
+    	exit()
+
     current_directory = os.getcwd()
-    nyu_path = 'data/nyu_depth_v2_labeled.mat'
-    convert_nyu(nyu_path)
+    train_path = os.path.join("data", sys.argv[1] ".npz")
+    convert_train(train_path)
