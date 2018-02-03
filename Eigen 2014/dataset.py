5168e576181c7c769dfdc136a84efde3c259089b
Unknown <hej.mellor@gmail.com>
2018-02-03 17:58:58 +0000

    change image decoder

--- Eigen 2014/dataset.py
+++ Eigen 2014/dataset.py
@@ -45,11 +45,11 @@ class DataSet:
         filename, depth_filename = tf.decode_csv(serialized_example, [["path"], ["annotation"]])
         # input
         jpg = tf.read_file(filename)
-        image = tf.image.decode_jpeg(jpg, channels=3)
+        image = tf.image.decode_image(jpg, channels=3)
         image = tf.cast(image, tf.float32)
         # target
         depth_png = tf.read_file(depth_filename)
-        depth = tf.image.decode_png(depth_png, channels=1)
+        depth = tf.image.decode_image(depth_png, channels=1)
         depth = tf.cast(depth, tf.float32)
         depth = tf.div(depth, [255.0])
         #depth = tf.cast(depth, tf.int64)
