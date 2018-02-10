dbf660169cc77e08c3ff576998bf7cca00ac78e7
Unknown <hej.mellor@gmail.com>
2018-02-10 16:17:18 +0000

    Unused line of code

--- Eigen 2014/convert_npz_to_img.py
+++ Eigen 2014/convert_npz_to_img.py
@@ -37,7 +37,6 @@ if __name__ == '__main__':
     if len(sys.argv) != 2:
     	print("Please run:\n\tpython convert_npz_to_img.py <train/val/test>")
     	exit()
-    current_directory = os.getcwd()
     train_path = os.path.join("../data", "%s.npz" % (sys.argv[1]))
     if not gfile.Exists('data/datasets_%s' % (sys.argv[1])):
         gfile.MakeDirs('data/datasets_%s' % (sys.argv[1]))
