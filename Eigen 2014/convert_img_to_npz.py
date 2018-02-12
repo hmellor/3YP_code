05bab1ab6b3c0449f44dd353ed1a897ff8726e47
Unknown <hej.mellor@gmail.com>
2018-02-12 18:47:10 +0000

    Writing in original img to npz

--- Eigen 2014/convert_img_to_npz.py
+++ Eigen 2014/convert_img_to_npz.py
@@ -1,7 +1,22 @@
+import tensorflow as tf
+import os
+import numpy as np
 
+def convert_predictions()
+    path = sys.argv[1]
+    images = os.listdir(path)
 
-
-    predictions = tf.image.resize_images(depths,[480,640])
-    predictions = tf.squeeze(predictions,3)
-    predictions = tf.transpose(predictions, [1,2,0])
+    for i, image in enumerate(zip(images))
+        depth_png = tf.read_file(image)
+        depth = tf.image.decode_png(depth_png, channels=1)
+        prediction = tf.image.resize_images(depth,[480,640])
+        prediction = tf.squeeze(predictions,3)
+        prediction = tf.transpose(predictions, [1,2,0])
     np.savez('%s.npz' % (output_dir), depths=predictions)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+    	print("Please run:\n\tpython convert_npz_to_img.py <predictions_path>")
+    	exit()
+    path = sys.argv[1]
+    convert_train()
