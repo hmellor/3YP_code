3a66e93712380c76270ff1c3ee9a39f11ea0c3f0
Unknown <george.barnett.121@gmail.com>
2018-02-12 18:05:45 +0000

    more  changes to append

--- convert_img_to_npz.py
+++ convert_img_to_npz.py
@@ -12,19 +12,36 @@ def img_to_npz(input_depth_directory, output_depth_directory):
     #Read depths
     depths = []
     
+    depths0 = []
+    depths1 = []
+    depths2 = []
+    
+    
+    
     print("load depth dataset: %s" % (input_depth_directory))
     
     #assuming all png images here are predicted depths
     for filename in glob.glob('%s/*.png' % input_directory): 
         im = tf.image.decode_jpeg(filename, channels=1)
         predictions = tf.image.resize_images(im,[55,74])
-        predictions = tf.transpose(predictions, [1,2,0])    
-        depths = np.append(depths,predictions, axis=0)
+        predictions = tf.transpose(predictions, [1,2,0])
+        depths0 = np.append(depths,predictions, axis=0)
+        depths1 = np.append(depths,predictions, axis=1)
+        depths2 = np.append(depths,predictions, axis=2)
+        
         
   
-    #print(depths)
-    depths = np.asarray(depths)
-    print(depths.shape)
+    #print(depths   
+    
+    depths0 = np.asarray(depths0)
+    print(depths0.shape)
+    
+    depths1 = np.asarray(depths1)
+    print(depths1.shape)
+    
+    depths2 = np.asarray(depths2)
+    print(depths2.shape)
+    
     #np.savez('predictions', depths=depths)
     np.savez('predictions', depths=depths)
 
