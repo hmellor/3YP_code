0b1ff9b0fe5047211907ad2774cb8e9fa6f228eb
Unknown <hej.mellor@gmail.com>
2018-02-13 11:00:49 +0000

    == syntax

--- Eigen 2014/convert_img_to_npz.py
+++ Eigen 2014/convert_img_to_npz.py
@@ -3,9 +3,11 @@ import sys, glob, imageio
 import numpy as np
 
 def convert_predictions():
-    path = sys.argv[1]
-    for i, image in enumerate(glob.glob('%s/*.png' % path)):
+
+    # For all png images in arg 1 folder
+    for i, image in enumerate(glob.glob('%s/*.png' % sys.argv[1])):
         print(image)
+        # Read and resize image
         prediction = imageio.imread(image)
         prediction = tf.expand_dims(prediction, 2)
         print(prediction.shape)
@@ -13,17 +15,24 @@ def convert_predictions():
         print(prediction.shape)
         prediction = tf.squeeze(prediction,2)
         print(prediction.shape)
-        if i = 1:
+
+        # If this is the 1st image, create predictions
+        # and add dimension for image number
+        if i == 1:
             predictions = prediction
             predictions = tf.expand_dims(predictions, 2)
+            print(predictions.shape)
+        # Else append the current image to the predictions array
         else
             predictions = np.append(predictions, prediction, axis = 2)
-        print(predictions.shape)
+            print(predictions.shape)
+
+    # Save the entire array as a .npz
     np.savez('predictions.npz', depths=predictions)
 
 if __name__ == '__main__':
+    # Check that argument has been provided
     if len(sys.argv) != 2:
     	print("Please run:\n\tpython convert_npz_to_img.py <predictions_path>")
     	exit()
-    path = sys.argv[1]
     convert_predictions()
