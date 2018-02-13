92236e91fd3e3ea85bec0aa136de9784b46733be
Unknown <hej.mellor@gmail.com>
2018-02-13 11:49:42 +0000

    tensor to numpy array

--- Eigen 2014/convert_img_to_npz.py
+++ Eigen 2014/convert_img_to_npz.py
@@ -4,31 +4,33 @@ import numpy as np
 
 def convert_predictions():
 
-    # For all png images in arg 1 folder
-    for i, image in enumerate(glob.glob('%s/*[0-9].png' % sys.argv[1])):
-        print(image)
-        # Read and resize image
-        prediction = imageio.imread(image)
-        prediction = tf.expand_dims(prediction, 2)
-        #rint(prediction.shape)
-        prediction = tf.image.resize_images(prediction,[480,640])
-        #print(prediction.shape)
+    sess = tf.Session()
+    with sess.as_default():
+        # For all png images in arg 1 folder
+        for i, image in enumerate(glob.glob('%s/*[0-9].png' % sys.argv[1])):
+            print(image)
+            # Read and resize image (currently in tensor form)
+            prediction = imageio.imread(image)
+            prediction = tf.expand_dims(prediction, 2)
+            prediction = tf.image.resize_images(prediction,[480,640])
 
-        # If this is the 1st image, initialise predictions
-        if i == 0:
-            predictions = prediction
-            print(predictions.shape)
-        # Else append the current image to the predictions array
-        else:
-            print('predictions:')
-            print(predictions.shape)
-            print('prediction:')
-            print(prediction.shape)
-            predictions = tf.concat([predictions, prediction], 2)
-            print(predictions.shape)
+            # If this is the 1st image, initialise predictions
+            if i == 0:
+                predictions = prediction
+                print(predictions.shape)
+            # Else append the current image to the predictions array
+            else:
+                print('predictions:')
+                print(predictions.shape)
+                print('prediction:')
+                print(prediction.shape)
+                predictions = tf.concat([predictions, prediction], 2)
+                print(predictions.shape)
 
-    # Save the entire array as a .npz
-    np.savez('predictions.npz', depths=predictions)
+        # Convert tensor to numpy array
+        predictions = predictions.eval()
+        # Save the entire array as a .npz
+        np.savez('predictions.npz', depths=predictions)
 
 if __name__ == '__main__':
     # Check that argument has been provided
