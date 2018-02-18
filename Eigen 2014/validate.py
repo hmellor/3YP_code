a3325d74850a84c56d3d8103742625043344f68a
Unknown <george.barnett.121@gmail.com>
2018-02-18 13:52:57 +0000

    Still trying

--- Eigen 2014/validate.py
+++ Eigen 2014/validate.py
@@ -13,12 +13,13 @@ def predict(model_path, input_directory, output_directory):
         width = 304
         channels = 3
         batch_size = 1
-    
-        filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once(input_directory+"/00179.jpg"))
+        
+        print(input_directory+"*.jpg")
+        filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once(input_directory+"*.jpg"))
         image_reader = tf.WholeFileReader()
         _, image_file = image_reader.read(filename_queue)
         images = tf.image.decode_jpeg(image_file)
-        images.Print() 
+         
     
     
     
@@ -37,7 +38,7 @@ def predict(model_path, input_directory, output_directory):
         #images = tf.stack(imageslist)
         
         #images = tf.transpose(images, [1,2,3,0] ) # sort image stack (tensor) into proper dimensions, height, wdith, channels, image_id
-        print('\n** Loaded ' + str(images.shape()) + ' images. ** \n')
+        print('\n** Loaded ' + str(images.get_shape()) + ' images. ** \n')
     
     
         # Create a placeholder for the input image
