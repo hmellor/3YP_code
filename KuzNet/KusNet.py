depths transpose was in the wrong order

KuzNet/KusNet.py
@@ -69,7 +69,7 @@ def main():
 
     #rearrange into proper columns
     images_np = np.transpose(images_np, [3,0,1,2])
-    depths_np = np.transpose(depths_np, [2, 1, 0])
+    depths_np = np.transpose(depths_np, [2, 0, 1])
     #expand depths_np to have a single colour channel
     depths_np = np.expand_dims(depths_np, 3)
     #use opencv to resize all images in depths_np