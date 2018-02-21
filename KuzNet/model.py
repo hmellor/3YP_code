00fed80acc6325e865c4f359b58897559a991248
AkinOxford <32651958+AkinOxford@users.noreply.github.com>
2018-02-21 11:21:48 +0000

    got up to up-projection block1

--- KuzNet/model.py
+++ KuzNet/model.py
@@ -23,6 +23,7 @@ img_aug.add_random_flip_leftright()
 net = tflearn.input_data(shape=[None, 640, 480, 3],
                          data_augmentation=img_aug)
 
+#Main model section 1
 #first layer is a 2d convolution of size 7 and stride 2
 net = tflearn.conv_2d(net, 7, 3, strides=2)
 
@@ -30,4 +31,36 @@ net = tflearn.conv_2d(net, 7, 3, strides=2)
 #unsure if we need padding and what exactly is batch normalisation
 net = tflearn.layers.conv.max_pool_2d (net, 3, strides=2)
 
+#Main model section 2
+net = res1(net,2) #type 1, stride 2     resblock1
+net = res1(net,1) #type 1, stride 1
+net = res1(net,1) #type 1, stride 1
+resblock3 = net
+net = res2(net,2) #type 2, stride 2     resblock 4
+
+#Main model section 3
+net = res1(net,1) #type 1, stride 1
+net = res1(net,1) #type 1, stride 1
+net = res1(net,1) #type 1, stride 1
+resblock7 = net
+net = res2(net,2) #type 2, stride 2       resblock 8
+
+
+#Main model section 4
+net = res1(net,1) #type 1, stride 1       resblock 9 
+net = res1(net,1) #type 1, stride 1
+net = res1(net,1) #type 1, stride 1
+net = res1(net,1) #type 1, stride 1
+net = res1(net,1) #type 1, stride 1       resblock 13
+resblock13 = net
+net = res2(net,2) #type 2, stride 2       resblock 14
+
+
+#Main model section 5
+net = res1(net,1) #type 1, stride 1       resblock 15 
+net = res1(net,1) #type 1, stride 1
+
+
+#Main model section 6
+net = resup(net,1) #type 1, stride 1       upproject1 
 
