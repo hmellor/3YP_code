f27118e7888d3ece0437fbd332b1737ab4ed58f3
Unknown <george.barnett.121@gmail.com>
2018-02-21 16:50:47 +0000

    Renamed Stride in Custom Blocks to stride_size

--- KuzNet/custom_blocks.py
+++ KuzNet/custom_blocks.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import tflearn
 
 # Define type 1 residual block
-def res1(incoming, stride):
+def res1(incoming, stride_size):
     # First convolution that uses res1 inputs
     net = tflearn.layers.conv.conv_2d (
-        incoming=incoming, nb_filter=1, filter_size=1, stride=stride, padding='same',
+        incoming=incoming, nb_filter=1, filter_size=1, stride=stride_size, padding='same',
         activation='relu', bias=True, weights_init='truncated_normal',
         bias_init='zeros', regularizer=None, weight_decay=0.001, trainable=True,
         restore=True, reuse=False, scope=None, name='Type1 Conv2D 1')
@@ -29,10 +29,10 @@ def res1(incoming, stride):
     return net
 
 # Define type 2 residual block
-def res2(incoming, stride):
+def res2(incoming, stride_size):
     # First convolution that uses res2 inputs
     net = tflearn.layers.conv.conv_2d (
-        incoming=incoming, nb_filter=1, filter_size=1, stride=stride, padding='same',
+        incoming=incoming, nb_filter=1, filter_size=1, stride=stride_size, padding='same',
         activation='relu', bias=True, weights_init='truncated_normal',
         bias_init='zeros', regularizer=None, weight_decay=0.001, trainable=True,
         restore=True, reuse=False, scope=None, name='Type2 Conv2D 1')
@@ -50,7 +50,7 @@ def res2(incoming, stride):
         restore=True, reuse=False, scope=None, name='Type2 Conv2D 3')
     # Residual convolution that uses res2 unputs
     res = tflearn.layers.conv.conv_2d (
-        incoming=incoming, nb_filter=1, filter_size=1, stride=stride, padding='same',
+        incoming=incoming, nb_filter=1, filter_size=1, stride=stride_size, padding='same',
         activation='linar', bias=True, weights_init='truncated_normal',
         bias_init='zeros', regularizer=None, weight_decay=0.001, trainable=True,
         restore=True, reuse=False, scope=None, name='Type2 Conv2D res')
@@ -62,13 +62,13 @@ def res2(incoming, stride):
     return net
 
 # Define up project residual block
-def resup(incoming, stride):
+def resup(incoming, stride_size):
     # Upsample by a factor of 2
     net = tflearn.layers.conv.upsample_2d (
         incoming=incoming, kernel_size=2, name='resup UpSample2D')
     # Resudual convolution using upsample as input
     res = tflearn.layers.conv.conv_2d (
-        net, nb_filter=1, filter_size=5, stride=stride, padding='same',
+        net, nb_filter=1, filter_size=5, stride=stride_size, padding='same',
         activation='linear', bias=True, weights_init='truncated_normal',
         bias_init='zeros', regularizer=None, weight_decay=0.001, trainable=True,
         restore=True, reuse=False, scope=None, name='resup Conv2D res')
