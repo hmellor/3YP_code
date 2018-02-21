47899a1919852b676531f95dd2d0aab2fc17175c
AkinOxford <32651958+AkinOxford@users.noreply.github.com>
2018-02-21 11:50:58 +0000

    model update

--- KuzNet/model.py
+++ KuzNet/model.py
@@ -17,7 +17,7 @@ net = tflearn.input_data(shape=[None, 640, 480, 3],
                          data_augmentation=img_aug)
 
 #Main model section 1
-#first layer is a 2d convolution of size 7 and stride 2
+#first layer is a 2d convolution of size 7 and stride 2 and 3 channels
 net = tflearn.conv_2d(net, 7, 3, strides=2)
 
 # second layer is a maxpool layer of size 3 and stride 2
@@ -51,9 +51,21 @@ net = res2(net,2) #type 2, stride 2       resblock 14
 
 #Main model section 5
 net = res1(net,1) #type 1, stride 1       resblock 15 
-net = res1(net,1) #type 1, stride 1
-
+net = res1(net,1) #type 1, stride 1       resblock 16 
+#conv layer is a 2d convolution of size 1, stride 1 and 2048 channels
+net = tflearn.conv_2d(net, 1, 2048, strides=1)
 
 #Main model section 6
 net = resup(net,1) #type 1, stride 1       upproject1 
+
+#Main model - remaining up projections
+net = resup(net + resblock13) #type 1, stride 1       upproject2 
+net = resup(net + resblock7) #type 1, stride 1        upproject3 
+net = resup(net + resblock3) #type 1, stride 1        upproject4 
+
+
+#final conv layer is a 2d convolution of size 3, stride 1 and 64 channels
+net = tflearn.conv_2d(net, 64, 2048, strides=1)
+
+
 
