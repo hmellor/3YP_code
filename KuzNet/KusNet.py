ef8f62fad874d5714c6e64afeb474154843b567d
Unknown <hej.mellor@gmail.com>
2018-02-22 10:42:40 +0000

    Testing turning back to numpy array

--- KuzNet/KusNet.py
+++ KuzNet/KusNet.py
@@ -72,9 +72,10 @@ def main():
     depths_np = np.transpose(depths_np, [2, 0, 1])
     #expand depths_np to have a single colour channel
     depths_np = np.expand_dims(depths_np, 3)
-    #resize depths to 240x320
-    depths_np = tflearn.layers.conv.max_pool_2d (depths_np, 2, strides=2)
-    depths_np = np.reshape(depths_np,(-1,240,320,1))
+    #resize depths to 240x320 by halfing resolution using max pool
+    depths_tf = tflearn.layers.conv.max_pool_2d (depths_np, 2, strides=2)
+    depths_np = np.asarray(depths_tf)
+
     print(images_np.shape)
     print(depths_np.shape)
     print('\n ** %s images loaded successfully ** \n' % (images_np.shape[0]))
