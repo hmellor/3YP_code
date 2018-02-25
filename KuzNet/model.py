eafcf2a820ec2ae5a3eb673ac0b1fd3391df1f0f
Unknown <hej.mellor@gmail.com>
2018-02-25 15:41:43 +0000

    test custom loss

--- KuzNet/model.py
+++ KuzNet/model.py
@@ -81,11 +81,15 @@ def model_network():
     bias_init='zeros', regularizer='L2',
     weight_decay=wd, name='conv3')
     # No normalisation
-
+    loss = loss
     #Regression
     opt = tflearn.optimizers.Adam (learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8, use_locking=False, name='Adam')
     r2 = tflearn.metrics.R2()
     net = tflearn.layers.estimator.regression (
-        net, metric=r2, optimizer=opt, loss='mean_square')
+        net, metric=r2, optimizer=opt, loss='supervised_loss')
 
     return net
+
+def supervised_loss(y_pred, y_true):
+    with tf.name_scope("MeanSquare"):
+        return tf.reduce_mean(tf.square(y_pred - y_true))
