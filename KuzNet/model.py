d59f0b6fb74d74d162975e99805e09e3ec5ced4e
Unknown <hej.mellor@gmail.com>
2018-02-25 15:42:37 +0000

    remove loss=loss

--- KuzNet/model.py
+++ KuzNet/model.py
@@ -81,7 +81,7 @@ def model_network():
     bias_init='zeros', regularizer='L2',
     weight_decay=wd, name='conv3')
     # No normalisation
-    loss = loss
+
     #Regression
     opt = tflearn.optimizers.Adam (learning_rate=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8, use_locking=False, name='Adam')
     r2 = tflearn.metrics.R2()
