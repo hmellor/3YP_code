2c2ccfab0ed291968f73dc790a6a85d63b06207c
Unknown <hej.mellor@gmail.com>
2018-02-25 13:47:20 +0000

    img data naming scheme

--- KuzNet/convert_npz_to_img.py
+++ KuzNet/convert_npz_to_img.py
@@ -1,12 +1,16 @@
 #encoding: utf-8
-import os, random, sys
+import os, random, sys, datetime
 import numpy as np
 from PIL import Image
 
+def convert_train(data_path):
+    print("load dataset: %s" % (data_path))
 
-def convert_train(path):
-    print("load dataset: %s" % (path))
-    f = np.load(path)
+    #manipulate argument for file naming
+    file_name, ext = path.splitext(data_path)
+    time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
+
+    f = np.load(data_path)
     depths = f['depths']
     depths = np.transpose(depths, [2, 1, 0])
 
@@ -14,7 +18,7 @@ def convert_train(path):
         ra_depth = depth.transpose(1, 0)
         re_depth = (ra_depth/np.max(ra_depth))*255.0
         depth_pil = Image.fromarray(np.uint8(re_depth))
-        depth_name = os.path.join("data", "%05d.png" % (i))
+        depth_name = os.path.join("data", "%s_%s" % (file_name, time_str), "%05d.png" % (i))
         depth_pil.save(depth_name)
 
 if __name__ == '__main__':
